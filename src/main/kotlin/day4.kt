9e38b43df01f0c5e0307c5263ecbcb7ec3249a5e
Paul_B <paul_b@3vresearch.awsapps.com>
2019-12-04 22:38:30 +0000

    That's nicer. All sins are hidden in a util function with a fun name.
    no one actually looks in util functions anyway ;)

--- src/main/kotlin/day4.kt
+++ src/main/kotlin/day4.kt
@@ -1,17 +1,9 @@
 
-fun hasPairedDigits (digitString : String) : Boolean =
-    digitString.fold(FoldData('?', 1, false)) { acc, char ->
-        if (acc.lastChar == char) {
-            FoldData(char, acc.seenInARow + 1, acc.pairSpotted)
-        } else {
-            FoldData(char, 1, acc.pairSpotted || acc.seenInARow == 2)
-        }
-    }.let {
-        it.pairSpotted || it.seenInARow == 2
-    }
+fun hasPairedDigits (password : String) : Boolean =
+    password.toList().chunkOnChange().any { it.size == 2 }
 
-fun doesntDecrease (digitString: String) : Boolean =
-    digitString.windowed(2).none { it[0].toInt() > it[1].toInt() }
+fun doesntDecrease (password: String) : Boolean =
+    password.windowed(2).none { it[0].toInt() > it[1].toInt() }
 
 fun main() {
     // sort of cheating not parsing the input, but this code is not improved by the extra regex
@@ -21,5 +13,3 @@ fun main() {
 
     println(validPasswords)
 }
-
-data class FoldData(val lastChar: Char, val seenInARow: Int, val pairSpotted: Boolean)
--- src/main/kotlin/utils.kt
+++ src/main/kotlin/utils.kt
@@ -6,7 +6,27 @@ fun readTextFromFile(fileName: String): String = readFile(fileName).readText()
 
 private fun readFile(fileName: String) = File("src/main/resources/$fileName")
 
+
+//List fun
+/**
+ * combines 2 iterables into an iterable of Pairs with every combination of the two arguments
+ */
 fun <S, T> productOf(iter1 : Iterable<S>, iter2: Iterable<T>) : Iterable<Pair<S, T>> =
     iter1.flatMap{ s ->
         iter2.map { t -> Pair(s, t) }
+    }
+
+/**
+ * chunks a list in to sub lists for every change in adjacent elements
+ * [ a a b a c c d ] -> [ [ a a ] [ b ] [ a ] [ c c ] [ d ] ]
+ */
+fun <T> List<T>.chunkOnChange (): List<List<T>> =
+    this.fold(mutableListOf<MutableList<T>>()) { chunks, next ->
+        if (chunks.lastOrNull()?.contains(next) == true) {
+            chunks.last().add(next)
+            chunks
+        } else {
+            chunks += mutableListOf(next)
+            chunks
+        }
     }
