0f5c40c1b8478d0746171f51e533baf01e2f4a55
paulbeecroft <paul.beecroft@gmail.com>
2019-12-09 16:26:00 +0000

    day 8. doubt Ill bother refactoring that one

---
+++ src/main/kotlin/day8.kt
@@ -0,0 +1,32 @@
+import java.lang.IllegalStateException
+
+fun main() {
+    val encrpytedInput = readTextFromFile("day8.txt").map { it.toString().toInt() }
+
+    val layerSize = 25*6
+
+    val layers = encrpytedInput.chunked(layerSize)
+    val answer = layers
+        .minBy { layer -> layer.count { it == 0 } }
+        ?.let { layer -> layer.count { it == 1 } * layer.count { it == 2 } }
+    println(answer)
+
+    val pixels = (0 until layers[0].size)
+        .map { index -> layers.map { it[index] } }
+        .map { blackOrWhite(it) }
+
+    pixels.chunked(25)
+        .forEach { println(it.joinToString("")) }
+
+}
+
+fun blackOrWhite(pixelLayers: List<Int>) =
+    pixelLayers.first { it != 2 }
+        .let {
+            when (it) {
+                0 -> " "
+                1 -> "X"
+                else -> throw IllegalStateException("Crazy pixel")
+            }
+        }
+
