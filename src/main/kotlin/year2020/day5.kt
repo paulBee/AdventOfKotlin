fc14880d7f287fee7da60c1d7c6450060ca60115
Paul B <paul.beecroft@gmail.com>
2020-12-05 13:44:19 +0000

    simplifying day 5

--- src/main/kotlin/year2020/day5.kt
+++ src/main/kotlin/year2020/day5.kt
@@ -9,19 +9,19 @@ import toIntUsingDigitsOf
 fun main() {
     val sortedIds = readLinesFromFile("2020/day5.txt")
         .map { it.splitSize(7) }
-        .map{ (rowStr, seatStr) -> Pair(rowParser(rowStr), seatParser(seatStr)) }
-        .map{ (row, seat) -> boardingId(row, seat)}
-        .sorted()
+        .map { (rowStr, seatStr) -> Pair(rowParser(rowStr), seatParser(seatStr)) }
+        .map { (row, seat) -> boardingId(row, seat)}
+        .sorted().also { displayPart1(it.last()) }
 
-    val min = sortedIds.first()
-    val max = sortedIds.last().also(displayPart1)
-
-    val firstMissingNumber = (min..max zip sortedIds).first { (x, y) -> x != y }.first
-
-    displayPart2(firstMissingNumber)
+    sortedIds
+        .zipWithNext()
+        .first { (first, second) -> second - first == 2 }
+        .also { displayPart2(it.first + 1)}
 }
 
 val rowParser = toIntUsingDigitsOf('F', 'B')
 val seatParser = toIntUsingDigitsOf('L', 'R')
 
+// just realised the whole thing is a binary number :man_facepalming:
+// oh well the number parsing would have to take multiple characters per digit to support that which feels less useful
 fun boardingId(row: Int, seat: Int) = row * 8 + seat
