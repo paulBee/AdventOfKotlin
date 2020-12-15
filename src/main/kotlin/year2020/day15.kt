a8b7231ce5a66ce01404589108a7f72a825dfce4
Paul B <paul.beecroft@gmail.com>
2020-12-15 14:45:20 +0000

    day 15

---
+++ src/main/kotlin/year2020/day15.kt
@@ -0,0 +1,31 @@
+package year2020
+
+import utils.aoc.displayPart1
+import utils.aoc.displayPart2
+import utils.aoc.readTextFromFile
+
+fun main() {
+    val starter = readTextFromFile("2020/day15.txt").split(",").map { it.toLong() }
+
+    sequenceFrom(starter).take(2020).last().also(displayPart1)
+    sequenceFrom(starter).take(30_000_000).last().also(displayPart2)
+}
+
+fun sequenceFrom(startingList: List<Long>): Sequence<Long> {
+    val history = mutableMapOf<Long, Long>()
+    var iteration = 0L
+    var lastNumber = startingList[0]
+
+    return generateSequence()
+    {
+        if (iteration in startingList.indices) {
+            startingList[iteration.toInt()]
+        } else {
+            history[lastNumber]?.let { iteration - it } ?: 0L
+        }.also {
+            history[lastNumber] = iteration
+            lastNumber = it
+            iteration++
+        }
+    }
+}
