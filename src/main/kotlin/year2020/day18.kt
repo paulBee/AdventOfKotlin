0b43eb9576f27766bcfbb4dd9eca2c6292274fe0
Paul B <paul.beecroft@gmail.com>
2020-12-18 22:01:07 +0000

    day18 much simpler

--- src/main/kotlin/year2020/day18.kt
+++ src/main/kotlin/year2020/day18.kt
@@ -3,6 +3,7 @@ package year2020
 import utils.aoc.displayPart1
 import utils.aoc.displayPart2
 import utils.aoc.readLinesFromFile
+import utils.collections.multiply
 import utils.collections.sumLongBy
 
 fun main() {
@@ -10,51 +11,23 @@ fun main() {
     readLinesFromFile("2020/day18.txt").sumLongBy { parse2(it) }.also(displayPart2)
 }
 
-val parse1: (string: String) -> Long = expandBrackets(::operateFromLeft)
-val parse2: (string: String) -> Long = expandBrackets(expandAddition(::operateFromLeft))
+val parse1 = parseWithPrecedence(::fromLeft)
+val parse2 = parseWithPrecedence(::additionFirst)
 
-fun expandBrackets(then: (String) -> Long): (String) -> Long {
-
-    fun recurse(string: String): Long =
+fun parseWithPrecedence(then: (String) -> Long): (String) -> Long =
+    { string: String ->
         if (string.contains('(')) {
-
-            val indexOfLastBracket = string.indexOfFirst { it == ')' }
-            val startOfString = string.take(indexOfLastBracket + 1)
-
-            val indexOfStartBracket = startOfString.indexOfLast { it == '(' }
-            val innerBrackets =  startOfString.drop(indexOfStartBracket)
-            val simplerString = string.replace(innerBrackets, then(innerBrackets.drop(1).dropLast(1)).toString())
-
-            recurse(simplerString)
+            val innerExpression = string.takeWhile { it != ')' }.takeLastWhile { it != '(' }
+            val simplerString = string.replace("($innerExpression)", then(innerExpression).toString())
+            parseWithPrecedence(then)(simplerString)
         } else {
             then(string)
         }
+    }
 
-    return ::recurse
-}
+fun additionFirst(string: String) = string.split(" * ").map { fromLeft(it) }.multiply()
 
-fun expandAddition(then: (String) -> Long): (String) -> Long {
-
-    fun recurse(string: String): Long =
-        if (string.contains('+')) {
-
-            val indexOfPlus = string.indexOfFirst { it == '+' }
-            val firstNumber = string.take(indexOfPlus - 1).takeLastWhile { it.isDigit() }
-            val secondNumber = string.drop(indexOfPlus + 2).takeWhile { it.isDigit() }
-
-            val simplerString = string.take(indexOfPlus - (1 + firstNumber.length)) +
-                    then("$firstNumber + $secondNumber").toString() +
-                    string.drop(indexOfPlus + 2 + secondNumber.length)
-
-            recurse(simplerString)
-        } else {
-            then(string)
-        }
-
-    return ::recurse
-}
-
-fun operateFromLeft(string: String): Long {
+fun fromLeft(string: String): Long {
     val bits = string.split(" ")
     val head = bits.first()
     val tail = bits.drop(1).chunked(2)
