b965a11eddc770dc7f20533c34c240a1ad20a2c7
Paul B <paul.beecroft@gmail.com>
2020-12-23 15:35:25 +0000

    day23 not the fastest I've had it at, but a nice balance between speed and readabilility

--- src/main/kotlin/year2020/day23.kt
+++ src/main/kotlin/year2020/day23.kt
@@ -3,64 +3,42 @@ package year2020
 import kotlinx.collections.immutable.*
 import utils.aoc.displayPart1
 import utils.aoc.displayPart2
-import utils.collections.dropP
-import utils.collections.takeP
 
 fun main() {
 
-    generateSequence(persistentListOf(3,2,6,5,1,9,4,7,8)) { it.popAndSnip().insertAt(9) }
-        .drop(100)
-        .first()
-        .let { it.takeLastWhile { it != 1 } + it.takeWhile { it != 1 } }
-        .let { it.joinToString("") }
+    runCups(listOf(3,2,6,5,1,9,4,7,8)).elementAt(100)
+        .second.startingAfter(1).takeWhile { it != 1 }.joinToString("")
         .also(displayPart1)
 
-    runCups(listOf(3,2,6,5,1,9,4,7,8) + (10..1_000_000))
-        .take(10_000_000)
-        .last().also { displayPart2(it.first.toLong() * it.second.toLong()) }
+    runCups(listOf(3,2,6,5,1,9,4,7,8) + (10..1_000_000)).elementAt(10_000_000)
+        .second.startingAfter(1).take(2).toList()
+        .also { (first, second) -> displayPart2(first.toLong() * second.toLong()) }
 
 }
 
-fun runCups(initial: List<Int>): Sequence<Pair<Int, Int>> {
-    val maxNumber = initial.max()
-    val numberNextTo: MutableMap<Int,Int> = (initial + initial.take(1)).zipWithNext().toMap().toMutableMap()
-    var current = 3
-    return sequence {
-        while (true) {
-            val snippet = generateSequence(current) { numberNextTo[it]!! }.drop(1).take(3).toList()
+fun runCups(initial: List<Int>): Sequence<Pair<Int, Map<Int, Int>>> {
 
-            val nextCurrent = numberNextTo[snippet.last()]!!
+    val rollingDecrementFrom = rollingDecrement(initial.maxOrNull()!!)
 
+    return generateSequence(initial.first() to initial.asMapToNext())
+    { (current, numberNextTo) ->
 
-            val insertPoint = generateSequence(current) { if (it == 1) maxNumber else it - 1}.drop(1).first { it !in snippet }
+        val snippet = numberNextTo.startingAfter(current).take(3)
+        val insertPoint = rollingDecrementFrom(current).first { it !in snippet }
 
-            numberNextTo[current] = nextCurrent
-            numberNextTo[snippet.last()] = numberNextTo[insertPoint]!!
-            numberNextTo[insertPoint] = snippet.first()
-            current = nextCurrent
+        val newCurrent = numberNextTo[snippet.last()]!!
 
-            val first = numberNextTo[1]!!
-            val second = numberNextTo[first]!!
+        val removeSnippet = current to numberNextTo[snippet.last()]!!
+        val attachSnippet = snippet.last() to numberNextTo[insertPoint]!!
+        val attachInsert = insertPoint to snippet.first()
 
-            yield(first to second)
-        }
+        newCurrent to numberNextTo + mapOf(removeSnippet, attachSnippet, attachInsert)
     }
 }
 
-fun PersistentList<Int>.popAndSnip() = Triple(this.first(), this.takeP(4).dropP(1), this.dropP(4) + this.first())
+private fun List<Int>.asMapToNext() = (this + this.take(1)).zipWithNext().toMap().toPersistentHashMap()
 
-fun Triple<Int, PersistentList<Int>, PersistentList<Int>>.insertAt(max: Int): PersistentList<Int> {
-    val (source, toInsert, insertIn) = this
+private fun rollingDecrement(maxNumber: Int): (Int) -> Sequence<Int> =
+    { start: Int -> generateSequence(start) { if (it == 1) maxNumber else it - 1 }.drop(1) }
 
-    val target = generateSequence(source) { if (it == 1) max else it - 1 }
-        .drop(1)
-        .first { insertIn.contains(it) }
-
-    return insertIn.takeWhileP { it != target } + listOf(target) + toInsert + insertIn.takeLastWhileP { it != target }
-}
-
-private fun <E> PersistentList<E>.takeWhileP(f: (E) -> Boolean) =
-    this.takeWhile(f).toPersistentList()
-
-private fun <E> PersistentList<E>.takeLastWhileP(f: (E) -> Boolean) =
-    this.takeLastWhile(f).toPersistentList()
+private fun Map<Int, Int>.startingAfter(seed: Int): Sequence<Int> = generateSequence(seed) { this[it]!! }.drop(1)
