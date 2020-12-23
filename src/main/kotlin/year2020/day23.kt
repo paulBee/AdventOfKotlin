03f4bed77f613bca7eac60d5e39e35644c12b595
Paul B <paul.beecroft@gmail.com>
2020-12-23 14:22:10 +0000

    day23 part2 solved, will tidy later. would like to combine the parts and yeild the map on each sequence

--- src/main/kotlin/year2020/day23.kt
+++ src/main/kotlin/year2020/day23.kt
@@ -15,14 +15,36 @@ fun main() {
         .let { it.joinToString("") }
         .also(displayPart1)
 
-    val input = persistentListOf(3,2,6,5,1,9,4,7,8) + (10..1_000_000)
+    runCups(listOf(3,2,6,5,1,9,4,7,8) + (10..1_000_000))
+        .take(10_000_000)
+        .last().also { displayPart2(it.first.toLong() * it.second.toLong()) }
 
-    generateSequence(input) { it.popAndSnip().insertAt(1_000_000) }
-        .mapIndexed { i, it -> it.also { println(i) }}
-        .drop(1_000_000)
-        .first()
-        .let { it.takeLastWhile { it != 1 } + it.takeWhile { it != 1 } }
-        .also { displayPart2(it.first() * it.drop(1).first()) }
+}
+
+fun runCups(initial: List<Int>): Sequence<Pair<Int, Int>> {
+    val maxNumber = initial.max()
+    val numberNextTo: MutableMap<Int,Int> = (initial + initial.take(1)).zipWithNext().toMap().toMutableMap()
+    var current = 3
+    return sequence {
+        while (true) {
+            val snippet = generateSequence(current) { numberNextTo[it]!! }.drop(1).take(3).toList()
+
+            val nextCurrent = numberNextTo[snippet.last()]!!
+
+
+            val insertPoint = generateSequence(current) { if (it == 1) maxNumber else it - 1}.drop(1).first { it !in snippet }
+
+            numberNextTo[current] = nextCurrent
+            numberNextTo[snippet.last()] = numberNextTo[insertPoint]!!
+            numberNextTo[insertPoint] = snippet.first()
+            current = nextCurrent
+
+            val first = numberNextTo[1]!!
+            val second = numberNextTo[first]!!
+
+            yield(first to second)
+        }
+    }
 }
 
 fun PersistentList<Int>.popAndSnip() = Triple(this.first(), this.takeP(4).dropP(1), this.dropP(4) + this.first())
