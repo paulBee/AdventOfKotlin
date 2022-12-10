1e53cc44b9c38007793d08824def6ded657ddcc2
Paul Beecroft <paulbeecroft@Pauls-MacBook-Pro-2.local>
2022-12-10 10:53:07 +0000

    day10

---
+++ src/main/kotlin/year2022/day10.kt
@@ -0,0 +1,37 @@
+package year2022
+
+import utils.aoc.readLinesFromFile
+
+fun main() {
+
+    val register = registerSequence(readLinesFromFile("2022/day10.txt"))
+    val clockAndRegister = clockSequence().zip(register)
+
+    val signalStrength = clockAndRegister.map { (c, r) -> c * r }
+    (20..220 step 40).sumOf { signalStrength.elementAt(it - 1) }.also { println(it) }
+
+    val spritePosition = register.map { listOf(it - 1, it, it + 1) }
+
+    val pixels = pixelSequence().zip(spritePosition)
+        .map { (pix, spr) -> if (spr.contains(pix))'#' else '.' }
+
+    pixels.chunked(40).forEach { println(it.joinToString("")) }
+
+}
+
+fun clockSequence() = generateSequence(1L) { it + 1 }
+fun pixelSequence() = generateSequence(0L) { (it + 1) % 40 }.take(40 * 6)
+
+fun registerSequence(lines: List<String>) = sequence<Long> {
+    var value = 1L
+    lines.forEach { line ->
+        when {
+            line == "noop" -> yield(value)
+            line.startsWith("addx ") -> {
+                yield(value)
+                yield(value)
+                value += line.drop(5).toLong()
+            }
+        }
+    }
+}
